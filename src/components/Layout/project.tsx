07033efd23a8af4035113ad25043a891f0e3b9b3
Muhammad Fauzan <m.fauzan.hb@gmail.com>
2024-07-28 17:17:41 +0700

    second commit

--- src/components/Layout/project.tsx
+++ src/components/Layout/project.tsx
@@ -1,7 +1,7 @@
 import { MdModeOfTravel } from "react-icons/md";
 import Project1 from "../../../public/project/project1.png";
 import Project2 from "../../../public/project/project2.png";
-import Project3 from "../../../public/project/project3.png";
+// import Project3 from "../../../public/project/project3.png";
 import Project4 from "../../../public/project/project4.png";
 import Project5 from "../../../public/project/project5.png";
 
@@ -19,23 +19,23 @@ const Project = () => {
     {
       name: "Portofolio",
       description:
-        "This travel website provides features for booking tickets, hotels and others with ease and comfort when ordering with the application",
+        "This is my first portfolio that I made by following the figma design with very good colors and placement",
       link: "https://github.com/irfanfirdaus/Project-1",
       icons: <MdModeOfTravel />,
       image: Project2,
       tech: ["HTML", "CSS", "Tailwind", "JavaScript"],
     },
+    // {
+    //   name: "Portofolio",
+    //   description:
+    //     "This travel website provides features for booking tickets, hotels and others with ease and comfort when ordering with the application",
+    //   link: "https://github.com/irfanfirdaus/Project-1",
+    //   icons: <MdModeOfTravel />,
+    //   image: Project3,
+    //   tech: ["HTML", "CSS", "Tailwind", "JavaScript"],
+    // },
     {
-      name: "Portofolio",
-      description:
-        "This travel website provides features for booking tickets, hotels and others with ease and comfort when ordering with the application",
-      link: "https://github.com/irfanfirdaus/Project-1",
-      icons: <MdModeOfTravel />,
-      image: Project3,
-      tech: ["HTML", "CSS", "Tailwind", "JavaScript"],
-    },
-    {
-      name: "Portofolio",
+      name: "Website Travel",
       description:
         "This travel website provides features for booking tickets, hotels and others with ease and comfort when ordering with the application",
       link: "https://github.com/irfanfirdaus/Project-1",
@@ -44,25 +44,28 @@ const Project = () => {
       tech: ["HTML", "CSS", "Tailwind", "JavaScript"],
     },
     {
-      name: "Portofolio",
+      name: "Website Travel",
       description:
-        "This travel website provides features for booking tickets, hotels and others with ease and comfort when ordering with the application",
+        "for an even better experience in registering to climb the mountain without having to come to the counter",
       link: "https://github.com/irfanfirdaus/Project-1",
       icons: <MdModeOfTravel />,
       image: Project5,
       tech: ["HTML", "CSS", "Tailwind", "JavaScript"],
     },
-
   ];
 
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center" id="project">
+    <div
+      className="min-h-screen flex flex-col justify-center items-center"
+      id="project">
       <h1 className="text-5xl font-bold">Project that i has been done </h1>
-      <div className="flex flex-col items-center justify-center gap-32 pt-32" >
+      <div className="flex flex-col items-center justify-center gap-32 pt-32">
         {data.map((item) => (
           <div
             key={item.name}
-            className="flex flex-col md:flex-row justify-center items-center  p-5 rounded-xl shadow-2xl space-y-5 md:space-y-0 md:space-x-5" data-aos="fade-up" data-aos-duration="2000">
+            className="flex flex-col md:flex-row justify-center items-center  p-5 rounded-xl shadow-2xl space-y-5 md:space-y-0 md:space-x-5"
+            data-aos="fade-up"
+            data-aos-duration="2000">
             <div className="">
               <div className="">
                 <p className="text-sm font-bold ">Featured Project</p>
